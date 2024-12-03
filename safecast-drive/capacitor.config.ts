45fc41d758193db6e3594fbca1c01f80e3bbf859
robouden <rob@safecast.org>
2024-12-03 22:44:13 +0900

    Test rob

---
+++ safecast-drive/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Safecast Drive',
+  webDir: 'www'
+};
+
+export default config;
